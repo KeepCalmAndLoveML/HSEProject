4cd3878600d1e346c7697d2785acbaefbe6316b3
KeepCalmAndLoveML <32905904+KeepCalmAndLoveML@users.noreply.github.com>
2019-03-10 10:27:16 +0300

    Horizontal List view

--- Source/FormsPrototype/FormsPrototype/FormsPrototype/Views/ItemDetailPage.xaml.cs
+++ Source/FormsPrototype/FormsPrototype/FormsPrototype/Views/ItemDetailPage.xaml.cs
@@ -6,19 +6,23 @@ using Xamarin.Forms.Xaml;
 using FormsPrototype.Models;
 using FormsPrototype.ViewModels;
 
+using HorizontalListView = Sharpnado.Presentation.Forms.RenderedViews.HorizontalListView;
+
+
 namespace FormsPrototype.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ItemDetailPage : ContentPage
 	{
 		ItemDetailViewModel viewModel;
-
+		HorizontalListView relatedItemsListView;
 
 		public ItemDetailPage(ItemDetailViewModel viewModel)
 		{
 			InitializeComponent();
 
-			BindingContext = this.viewModel = viewModel;
+			this.viewModel = viewModel;
+			Initialize();
 		}
 
 		public ItemDetailPage()
@@ -34,7 +38,30 @@ namespace FormsPrototype.Views
 			};
 
 			viewModel = new ItemDetailViewModel(item);
+			Initialize();		
+		}
+		
+		private void Initialize()
+		{
 			BindingContext = viewModel;
+
+			//Initialize this programmatically as XAML does not work for some reason
+			relatedItemsListView = new HorizontalListView();
+			relatedItemsListView.ItemsSource = viewModel.RelatedItems;
+			relatedItemsListView.IsVisible = true;
+			relatedItemsListView.ItemWidth = 144;
+			relatedItemsListView.ItemHeight = 144;
+
+			#region ItemTemplate
+
+			relatedItemsListView.ItemTemplate = new DataTemplate(() =>
+				{
+					return new ViewCell { View = new ItemView() };
+				}
+			);
+			
+
+			#endregion
 		}
 	}
 }
---
+++ Source/FormsPrototype/FormsPrototype/FormsPrototype/Views/ItemView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using FormsPrototype.Models;
+
+namespace FormsPrototype.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemView : ContentView
+	{
+		Item item;
+
+		public ItemView ()
+		{
+			InitializeComponent();
+
+			item = new Item
+			{
+				Name = "Shoes",
+				Description = "This is an item description.",
+				ShoeSize = 260,
+				Gender = Gender.Male
+			};
+
+			this.BindingContext = item;
+		}
+
+		public ItemView(Item item)
+		{
+			InitializeComponent();
+
+			this.BindingContext = this.item = item;
+		}
+	}
+}
