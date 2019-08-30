db16c74af51fa4a0708952c988dd6a8ed76e7403
Philippe GUYARD <abcdef0509@mail.ru>
2019-08-30 13:03:30 +0300

    Created the final app

---
+++ Source/RussianModnik/RussianModnik/RussianModnik/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RussianModnik.Models;
+
+namespace RussianModnik.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
